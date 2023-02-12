44619932207353f271c15f9da2aca50a48889a52
Uriel <sombrerero5016@gmail.com>
2023-02-11 18:01:36 -0800

    employee class initialized variables

---
+++ src/main/java/tech/getarrays/employeemanager/model/employee.java
@@ -0,0 +1,12 @@
+package tech.getarrays.employeemanager.model;
+
+public class employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
+    private String imageUrl;
+    private String employeeCode;
+
+}
