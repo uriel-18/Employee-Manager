bc0e70d9cc7d22c48c5109db5d1d012c7e708edd
Uriel <sombrerero5016@gmail.com>
2023-02-11 18:20:45 -0800

    employee class initialized variables

--- src/main/java/tech/getarrays/employeemanager/model/employee.java
+++ src/main/java/tech/getarrays/employeemanager/model/employee.java
@@ -1,12 +1,79 @@
 package tech.getarrays.employeemanager.model;
+import jakarta.persistence.*;
 
-public class employee {
+import java.io.Serializable;
+
+@Entity
+public class employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType. AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
     private String jobTitle;
-    private String phoneNumber;
+    private String phone;
     private String imageUrl;
+    @Column(nullable = false, updatable = false)
     private String employeeCode;
 
+    public employee() {}
+
+    public employee(String name, String email, String jobTitle, String phone, String imageUrl, String employeeCode) {
+        this.name= name;
+        this.email= email;
+        this.jobTitle= jobTitle;
+        this.phone= phone;
+        this.imageUrl= imageUrl;
+        this.employeeCode= employeeCode;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id=id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email){
+        this.email= email;
+    }
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone= phone;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl= imageUrl;
+    }
+    public String getEmployeeCode(){
+        return employeeCode;
+    }
+    public void setEmployeeCode(String employeeCode){
+        this.employeeCode= employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{"+
+                "id=" + id +
+                ", name= " + '\''+
+                ", email='"+ email + '\'' +
+                ", phone='"+ phone + '\'' +
+                ", imageUrl'" + imageUrl +'\''+
+                '}';
+    }
+
 }
+
